c755301f58a5950ea4778aced3a37f8d9d80c1b2
Aram Ceballos <aramgonzalez12@hotmail.com>
2021-12-09 13:07:23 -0700

    Refactor ListOfMovies component

--- src/components/ListOfMovies/index.tsx
+++ src/components/ListOfMovies/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import MovieInfoModal from '../MovieInfoModal'
 
 import { Container, ListTitle, Ul, ImageItem } from './styles'
@@ -7,52 +7,86 @@ type Props = {
   movies: IMovie[]
 }
 
-const ListOfMovies = ({ movies }: Props) => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const [movieInfo, setMovieInfo] = useState<IMovie | null>(null)
+type State = {
+  isModalOpen: boolean
+  movieInfo: IMovie
+}
 
-  const shuffledMovies = movies
-    .map((a) => ({ sort: Math.random(), value: a }))
-    .sort((a, b) => a.sort - b.sort)
-    .map((a) => a.value)
+class ListOfMovies extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+    this.handleClose = this.handleClose.bind(this)
 
-  const handleClick = (movie: IMovie) => {
-    setIsModalOpen(true)
-    setMovieInfo(movie)
+    this.state = {
+      isModalOpen: false,
+      movieInfo: {
+        adult: false,
+        backdrop_path: '',
+        genre_ids: [],
+        id: 0,
+        original_language: '',
+        original_title: '',
+        overview: '',
+        popularity: 0,
+        poster_path: '',
+        release_date: '',
+        title: '',
+        video: false,
+        vote_average: 0,
+        vote_count: 0,
+      },
+    }
   }
 
-  const handleClose = () => {
-    setIsModalOpen(false)
+  handleClick(movie: IMovie) {
+    this.setState({
+      isModalOpen: true,
+      movieInfo: movie,
+    })
   }
 
-  return (
-    <Container>
-      <ListTitle>Discover Movies</ListTitle>
-      <Ul>
-        {shuffledMovies.map((movie: IMovie) => {
-          if (!movie.backdrop_path) {
-            return
-          }
+  handleClose() {
+    this.setState({
+      isModalOpen: false,
+    })
+  }
 
-          return (
-            <li key={movie.id}>
-              <div onClick={() => handleClick(movie)}>
-                <ImageItem
-                  src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
-                  alt={movie.title}
-                />
-              </div>
-            </li>
-          )
-        })}
-      </Ul>
-      <MovieInfoModal
-        movie={movieInfo}
-        open={isModalOpen}
-        onClose={handleClose}
-      />
-    </Container>
-  )
+  render() {
+    const shuffledMovies = this.props.movies
+      .map((a) => ({ sort: Math.random(), value: a }))
+      .sort((a, b) => a.sort - b.sort)
+      .map((a) => a.value)
+
+    return (
+      <Container>
+        <ListTitle>Discover Movies</ListTitle>
+        <Ul>
+          {shuffledMovies.map((movie: IMovie) => {
+            if (!movie.backdrop_path) {
+              return
+            }
+
+            return (
+              <li key={movie.id}>
+                <div onClick={() => this.handleClick(movie)}>
+                  <ImageItem
+                    src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
+                    alt={movie.title}
+                  />
+                </div>
+              </li>
+            )
+          })}
+        </Ul>
+        <MovieInfoModal
+          movie={this.state.movieInfo}
+          open={this.state.isModalOpen}
+          onClose={this.handleClose}
+        />
+      </Container>
+    )
+  }
 }
 
 export default ListOfMovies
