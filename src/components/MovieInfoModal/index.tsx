69f167622c8682cb5f51a3f5dfba601d604c1869
Aram Ceballos <aramgonzalez12@hotmail.com>
2021-12-11 14:20:00 -0700

    Refactor MovieInfoModal component

--- src/components/MovieInfoModal/index.tsx
+++ src/components/MovieInfoModal/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Modal from '@mui/material/Modal'
 import Backdrop from '@mui/material/Backdrop'
 import Fade from '@mui/material/Fade'
@@ -22,52 +22,60 @@ import {
 } from './styles'
 
 type Props = {
-  movie: IMovie | null
+  movie: IMovie
   open: boolean
   onClose: () => void
 }
 
-const MovieInfoModal = ({ movie, open, onClose }: Props) => {
-  return (
-    <Modal
-      open={open}
-      onClose={onClose}
-      closeAfterTransition
-      BackdropComponent={Backdrop}
-      BackdropProps={{ timeout: 500 }}
-    >
-      <Fade in={open}>
-        <Container>
-          <CloseButton onClick={onClose}>
-            <CloseIcon />
-          </CloseButton>
-          <ImageContainer>
-            <Img
-              src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
-              alt="cover"
-              loading="lazy"
-            />
-            <ImageInfoContainer>
-              <Title>{movie?.title}</Title>
-              <ButtonsContainer>
-                <PlayButton>
-                  <Play width={20} />
-                  <ButtonText>Play</ButtonText>
-                </PlayButton>
-              </ButtonsContainer>
-            </ImageInfoContainer>
-          </ImageContainer>
-          <Shadow />
-          <InfoContainer>
-            <LeftContent>
-              <InfoText>{movie?.release_date.split('-')[0]}</InfoText>
-              <InfoText>{movie?.overview}</InfoText>
-            </LeftContent>
-          </InfoContainer>
-        </Container>
-      </Fade>
-    </Modal>
-  )
+class MovieInfoModal extends Component<Props> {
+  constructor(props: Props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <Modal
+        open={this.props.open}
+        onClose={this.props.onClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{ timeout: 500 }}
+      >
+        <Fade in={this.props.open}>
+          <Container>
+            <CloseButton onClick={this.props.onClose}>
+              <CloseIcon />
+            </CloseButton>
+            <ImageContainer>
+              <Img
+                src={`https://image.tmdb.org/t/p/original${this.props.movie.backdrop_path}`}
+                alt="cover"
+                loading="lazy"
+              />
+              <ImageInfoContainer>
+                <Title>{this.props.movie.title}</Title>
+                <ButtonsContainer>
+                  <PlayButton>
+                    <Play width={20} />
+                    <ButtonText>Play</ButtonText>
+                  </PlayButton>
+                </ButtonsContainer>
+              </ImageInfoContainer>
+            </ImageContainer>
+            <Shadow />
+            <InfoContainer>
+              <LeftContent>
+                <InfoText>
+                  {this.props.movie.release_date.split('-')[0]}
+                </InfoText>
+                <InfoText>{this.props.movie.overview}</InfoText>
+              </LeftContent>
+            </InfoContainer>
+          </Container>
+        </Fade>
+      </Modal>
+    )
+  }
 }
 
 export default MovieInfoModal
